573940707e9a845cb7cf7ddcdfe0a2cd3844efd9
castillin <castillo@mitcon.org>
2020-03-02 01:42:15 -0500

    Fixed source.uri Empty String

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,31 +1,29 @@
-import { useEffect, useState } from 'react';
-import yelp from '../api/yelp';
+import { useEffect, useState } from "react";
+import yelp from "../api/yelp";
 
 export default () => {
-    const [results, setResults] = useState([]);
-    const [errorMessage, setError] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setError] = useState("");
 
-    const searchApi = async (searchTerm) => {
-
-        try {
-            //searchTerm.persist()
-            const response = await yelp.get('/search', {
-
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'saginaw'
-                }
-            });
-            setResults(response.data.businesses)
-        } catch (err) {
-            setError('Something went wrong');
+  const searchApi = async searchTerm => {
+    try {
+      //searchTerm.persist();
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "lansing"
         }
-    };
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setError("Something went wrong");
+    }
+  };
 
-        useEffect(() => {
-            searchApi('pasta')
-        }, []);
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
 
-    return [searchApi, results, errorMessage]
+  return [searchApi, results, errorMessage];
 };
--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,41 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { Text, Image, View, StyleSheet, FlatList } from 'react-native';
-import yelp from '../api/yelp'
+import React, { useState, useEffect } from "react";
+import { Text, Image, View, StyleSheet, FlatList } from "react-native";
+import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation }) => {
-    const [result, setResult] = useState(null)
-    const id = navigation.getParam('id');
+  const [result, setResult] = useState(null);
+  const id = navigation.getParam("id");
 
+  const getResult = async id => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+  useEffect(() => {
+    getResult(id);
+  }, []);
 
-    const getResult = async ( id ) => {
-        const response = await yelp.get(`/${id}`)
-        setResult(response.data)
-    }
-    useEffect(() => {
-        getResult(id)
-    }, [])
+  if (!result) {
+    return null;
+  }
 
-    if (!result) {
-        return null
-    }
-
-    return <>
-        <Text>{result.name}</Text>
-        <FlatList
-            data={result.photos}
-            keyExtractor={(photo) => photo}
-            renderItem={({ item }) => {
-                return <Image style={styles.image} source={{ uri: item }} />
-            }}
-        />
-    </>
-}
+  return (
+    <View>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={photo => photo}
+        renderItem={({ item }) => {
+          console.log(result.photos);
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    image: {
-        height: 200,
-        width: 300,
-    }
-})
+  image: {
+    height: 200,
+    width: 300
+  }
+});
 
-export default ResultsShowScreen
+export default ResultsShowScreen;
